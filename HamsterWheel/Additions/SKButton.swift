99e6e5d392ace9a99426c490bff663dc6a405ad4
BobDeKort <dekort.bob101@gmail.com>
2018-01-22 18:37:59 -0800

    Initial commit With test menu

---
+++ HamsterWheel/Additions/SKButton.swift
@@ -0,0 +1,67 @@
+//
+//  SKButton.swift
+//  HamsterWheel
+//
+//  Created by Bob De Kort on 1/22/18.
+//  Copyright © 2018 Bob De Kort. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+enum SKButtonNodeState {
+    case active, selected, hidden
+}
+
+class SKButton: SKSpriteNode {
+    
+    /* Setup a dummy action closure */
+    var selectedHandler: () -> Void = { print("No button action set") }
+    
+    /* Button state management */
+    var state = SKButtonNodeState.active {
+        didSet {
+            switch state {
+            case .active:
+                /* Enable touch */
+                self.isUserInteractionEnabled = true
+                
+                /* Visible */
+                self.alpha = 1
+                break
+            case .selected:
+                /* Semi transparent */
+                self.alpha = 0.7
+                break
+            case .hidden:
+                /* Disable touch */
+                self.isUserInteractionEnabled = false
+                
+                /* Hide */
+                self.alpha = 0
+                break
+            }
+        }
+    }
+    
+    /* Support for NSKeyedArchiver (loading objects from SK Scene Editor */
+    required init?(coder aDecoder: NSCoder) {
+        
+        /* Call parent initializer e.g. SKSpriteNode */
+        super.init(coder: aDecoder)
+        
+        /* Enable touch on button node */
+        self.isUserInteractionEnabled = true
+    }
+    
+    // MARK: - Touch handling
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        state = .selected
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        selectedHandler()
+        state = .active
+    }
+    
+}
---
+++ HamsterWheel/Controllers/MainMenuScene.swift
@@ -0,0 +1,49 @@
+//
+//  MainMenuScene.swift
+//  HamsterWheel
+//
+//  Created by Bob De Kort on 1/22/18.
+//  Copyright © 2018 Bob De Kort. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class MainMenuScene: SKScene {
+    
+    /* UI Connections */
+    var playButtonGame1: SKButton!
+    var playButtonGame2: SKButton!
+    var playButtonGame3: SKButton!
+    
+    override func didMove(to view: SKView) {
+        /* Setup your scene here */
+        
+        /* Set UI connections */
+        playButtonGame1 = self.childNode(withName: "playButton1") as! SKButton
+        
+        /* Setup button selection handler */
+        playButtonGame1.selectedHandler = { [unowned self] in
+            if let view = self.view {
+                
+                // Load the SKScene from 'GameScene.sks'
+                if let scene = SKScene(fileNamed: "GameScene") {
+                    
+                    // Set the scale mode to scale to fit the window
+                    scene.scaleMode = .aspectFill
+                    
+                    // Present the scene
+                    view.presentScene(scene)
+                }
+                
+                // Debug helpers
+                view.showsFPS = true
+                view.showsPhysics = true
+                view.showsDrawCount = true
+            }
+        }
+        
+        playButtonGame2 = self.childNode(withName: "playButton2") as! SKButton
+        playButtonGame3 = self.childNode(withName: "playButton3") as! SKButton
+    }
+}
