00d115a5d8f33e575553a2a627600786e40459c8
Phyllis Wong <wong.phyllism@gmail.com>
2018-02-16 14:07:31 -0800

    testing login layout

---
+++ HamsterWheel/LoginController/LoginController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginController.swift
+//  HamsterWheel
+//
+//  Created by Phyllis Wong on 2/13/18.
+//  Copyright © 2018 Bob De Kort. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
