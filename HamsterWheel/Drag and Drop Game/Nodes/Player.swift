24f33393fc7e549189990433069521d1e0299f6f
Phyllis <wong.phyllism@gmail.com>
2018-03-13 14:13:42 -0700

    refactor player class to have Player1 and Player2 subclass from Player

--- HamsterWheel/Drag and Drop Game/Nodes/Player.swift
+++ HamsterWheel/Drag and Drop Game/Nodes/Player.swift
@@ -9,50 +9,52 @@
 import Foundation
 import SpriteKit
 
-class Player1: SKSpriteNode {
+class Player: SKSpriteNode {
     
     init(imageNamed: String) {
         let size = CGSize(width: 100, height: 100)
         let texture = SKTexture(imageNamed: imageNamed)
         
+        func setupPlayerPhysics() {
+            self.physicsBody = SKPhysicsBody(circleOfRadius: 50)
+            self.physicsBody?.affectedByGravity = false
+            self.physicsBody?.allowsRotation = false
+            self.physicsBody?.linearDamping = 0.7
+        }
+        
         super.init(texture: texture, color: .white, size: size)
-        setupPlayer1Physics()
+        setupPlayerPhysics()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
-    func setupPlayer1Physics() {
-        self.physicsBody = SKPhysicsBody(circleOfRadius: 50)
-        self.physicsBody?.affectedByGravity = false
-        self.physicsBody?.linearDamping = 0.7
-        self.physicsBody?.angularDamping = 0.5
+}
+
+class Player1: Player {
+    
+    override init(imageNamed: String) {
+        super.init(imageNamed: imageNamed)
         self.physicsBody?.categoryBitMask = PhysicsCategory.Player1
         self.physicsBody?.collisionBitMask = PhysicsCategory.Wall
         self.physicsBody?.contactTestBitMask = PhysicsCategory.Match1
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
 
-class Player2: SKSpriteNode {
+class Player2: Player {
     
-    init(imageNamed: String) {
-        let size = CGSize(width: 100, height: 100)
-        let texture = SKTexture(imageNamed: imageNamed)
-        super.init(texture: texture, color: .white, size: size)
-        setupPlayer2Physics()
-    }
-    
-    func setupPlayer2Physics() {
-        self.physicsBody = SKPhysicsBody(circleOfRadius: 50)
-        self.physicsBody?.affectedByGravity = false
-        self.physicsBody?.linearDamping = 0.7
-        self.physicsBody?.angularDamping = 0.5
+    override init(imageNamed: String) {
+        super.init(imageNamed: imageNamed)
         self.physicsBody?.categoryBitMask = PhysicsCategory.Player2
         self.physicsBody?.collisionBitMask = PhysicsCategory.Wall
         self.physicsBody?.contactTestBitMask = PhysicsCategory.Match2
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
